1ff31570ec5d0e70ec1739a217c51b094c992b3b
anushkachouhan029 <anushkachouhan029>
2022-12-11 18:20:41 +0530

    added new project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using DataStructure_Algorithm;
+
+namespace DataStructure_Algorithmls
+
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
+            {
+                Console.WriteLine("Welcome To Datastructures and Algorithms");
+                Console.WriteLine("1.Permutation Using Recursion\n2.BinarySearch");
+                Console.WriteLine("Enter Your Option");
+                int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        PermutationsOfString permutationsofstring = new PermutationsOfString();
+                        permutationsofstring.permutations();
+                        break;
+                    case 2:
+                        BinarySearchProb binarysearchprob = new BinarySearchProb();
+                        binarysearchprob.binarySearch();
+                        break;
+                    default:
+                        Console.WriteLine("-------Enter the valid option---");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to continue");
+                flag = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
